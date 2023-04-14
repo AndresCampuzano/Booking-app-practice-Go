1acbcb315000512762f696d67515a664e299c868
Andres Campuzano Garzon <campuxano88@gmail.com>
2023-04-14 18:38:29 -0500

    Asks user for their data

--- main.go
+++ main.go
@@ -13,13 +13,29 @@ func main() {
 	fmt.Printf("We have %v tickets remaining and %v tickets are available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get you tickets here to attend.")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
+
 	// ask user for name
+	fmt.Println("Please enter your name:")
+	fmt.Scan(&firstName)
 
-	userName = "John Doe"
-	userTickets = 2
-	fmt.Printf("Hello %v, welcome to %v\n", userName, conferenceName)
+	// ask user for last name
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+
+	// ask user for email
+	fmt.Println("Please enter your email:")
+	fmt.Scan(&email)
+
+	// ask user for number of tickets
+	fmt.Println("Please enter the number of tickets:")
+	fmt.Scan(&userTickets)
+
+	// print user information
+	fmt.Printf("Hello %v %v, welcome to %v\n", firstName, lastName, conferenceName)
 	fmt.Printf("You have booked %v tickets\n", userTickets)
-
+	fmt.Printf("A confirmation email has been sent to %v\n", email)
 }
